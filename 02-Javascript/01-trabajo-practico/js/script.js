6fcfd53d4495da71f893d2b9e4e5a630094710b9
Nicolas Grosso <nicolas.grosso@watea.com.ar>
2021-09-15 20:41:28 -0300

    Se agrega entrega modificando el dom para JS

--- 02-Javascript/01-trabajo-practico/js/script.js
+++ 02-Javascript/01-trabajo-practico/js/script.js
@@ -1,75 +1,92 @@
-//TODO: Agregar un array de elementos de tipo Producto predefinidos
-// Main del proyecto
-function main() {
+// Declaracion de variables globales
+let precioProductos = 0;
+let precioProductosConIva = 0;
+let arrayProductos = [];
+const MENSAJE_ERROR = 'Por favor, complete todos los campos';
 
-  // Declaracion de variables
-  let listadoProductos = 'Lista de productos:'
-  let precioProductos = 0;
-  let precioProductosConIva = 0;
+// Trigger del boton para agregar productos
+function agregarProducto() {
+  let nombreProducto = document.getElementById("nombre").value;
+  let precioProducto = document.getElementById("precio").value;
+  let cantidadProducto = document.getElementById("cantidad").value;
 
-  // Ingreso de productos al array
-  let arrayProductos = ingresarProductos();
+  // Condiciones para validar campos
+  if (nombreProducto === '' || precioProducto === '' || cantidadProducto === '') return alert(MENSAJE_ERROR);
 
-  // Uso del sort para ordenar los productos por precio neto
+  // Limpieza de campos
+  document.getElementById("nombre").value = '';
+  document.getElementById("precio").value = '';
+  document.getElementById("cantidad").value = '';
+
+  // Creacion de objeto producto y agregado a array
+  let producto = new Producto(nombreProducto, precioProducto, cantidadProducto);
+  console.log(producto);
+  arrayProductos.push(producto);
+  renderizarProducto(producto);
+  generarTicket();
+}
+
+// Renderiza producto en el HTML
+function renderizarProducto(producto) {
+  let divListaProductos = document.getElementById('listaProductos');
+
+  let divContenedor = document.createElement('div');
+  divContenedor.style.display = 'grid';
+  divContenedor.style.gridTemplateAreas = '"nombre precio cantidad borrar"';
+  divContenedor.style.gridTemplateColumns = '1fr 1fr 1fr 1fr';
+  divContenedor.style.gridTemplateRows = '1fr';
+  divContenedor.style.gridGap = '10px';
+  divContenedor.style.border = '1px solid black';
+  divContenedor.style.padding = '10px';
+
+  let divNombre = document.createElement('div');
+  divNombre.style.gridArea = 'nombre';
+  divNombre.innerText = producto.nombre;
+
+  let divPrecio = document.createElement('div');
+  divPrecio.style.gridArea = 'precio';
+  divPrecio.innerText = producto.precio;
+
+  let divCantidad = document.createElement('div');
+  divCantidad.style.gridArea = 'cantidad';
+  divCantidad.innerText = producto.cantidad;
+
+  let buttonBorrar = document.createElement('button');
+  buttonBorrar.style.gridArea = 'borrar';
+  buttonBorrar.innerText = 'X';
+  buttonBorrar.addEventListener('click', () => {
+    // Funcion para borrar un producto del array y del DOM
+    divListaProductos.removeChild(divContenedor);
+    let idBorrar = producto.id;
+    let index = 0;
+    arrayProductos.forEach(producto => {
+      if (producto.id === idBorrar) {
+        arrayProductos.splice(index, 1);
+      }
+      index++;
+    })
+    generarTicket();
+  });
+
+  divContenedor.appendChild(divNombre);
+  divContenedor.appendChild(divPrecio);
+  divContenedor.appendChild(divCantidad);
+  divContenedor.appendChild(buttonBorrar);
+
+  divListaProductos.appendChild(divContenedor);
+}
+
+function generarTicket() {
+  // Ordenamiento del array de productos por precio neto
   arrayProductos.sort((primerElemento, segundoElemento) => (primerElemento.getPrecioNeto() - segundoElemento.getPrecioNeto()));
 
   console.table(arrayProductos);
 
-  // Manejo del array de productos
-  arrayProductos.forEach(producto => {
-    //Se saca la asignacion ya que solo se usa para el alert
-    //listadoProductos += agregarListaProductos(producto);
-    precioProductos += producto.getPrecioNeto();
-    precioProductosConIva += aplicarIva(producto.getPrecioNeto());
-  });
-
-  console.log('Valor total de productos sin Iva ' + precioProductos);
-  console.log('Valor total de productos con Iva ' + precioProductosConIva);
-
-  /** TODO: Para la proxima entrega con el uso de eventos voy a hacer una 
-   * refactorizacion importante usando forms y eventos por ahora busco solo 
-   * usar el DOM para generar un ticket*/
-  crearTicket(arrayProductos);
-
-  //alert(listadoProductos + '\n\n Subtotal: ' + precioProductos.toFixed(2) + '\n IVA    +21% \n Total: ' + precioProductosConIva.toFixed(2));
-}
-
-// Funciones Auxiliares
-function ingresarProductos() {
-  let arrayProductos = [];
-  while (confirm('Desea ingresar producto?')) {
-
-    let producto = crearProducto();
-    console.log(producto);
-    //precioProductos += producto.precio;
-    //precioProductosConIva += producto.aplicarIva();
-    arrayProductos.push(producto);
-    //agregarListaProductos(producto);
-  }
-  return arrayProductos;
-}
-
-function crearProducto() {
-  let nombreProducto = prompt('Nombre del producto');
-  let precioProducto = prompt('Precio del producto');
-  let cantidadProducto = prompt('Cantidad del producto');
-
-  alert('Se ingreso el producto: \nNombre: ' + nombreProducto + '\nPrecio: $' + precioProducto + '\nCantidad: ' + cantidadProducto);
-
-  return new Producto(nombreProducto, precioProducto, cantidadProducto);
-}
-
-function agregarListaProductos(producto) {
-  return '\n· ' + producto.nombre + '  --  Cant: ' + producto.cantidad + "  --  $" + producto.getPrecioNeto();
-}
-
-function aplicarIva(precio) {
-  return precio * 0.21;
-}
-
-function crearTicket(arrayProductos) {
   let ticket = document.getElementById('ticket');
 
+  regeneraTicket(ticket);
+
+  // Revisa si existen productos en el array para generar ticket y crea o no los elementos li correspondientes
   if (arrayProductos.length > 0) {
     let ul = document.createElement('ul');
     arrayProductos.forEach(producto => {
@@ -78,10 +95,11 @@ function crearTicket(arrayProductos) {
       ul.appendChild(li);
     });
     ticket.appendChild(ul);
-  } else{
+  } else {
     ticket.innerText = 'No hay productos';
   }
 
+  // Calculo de precios subtotal, iva y total
   let subtotalElement = document.createElement('p');
   const subtotal = arrayProductos.reduce((total, producto) => total + producto.getPrecioNeto(), 0);
   subtotalElement.innerText = `Subtotal: $${subtotal}`;
@@ -94,13 +112,29 @@ function crearTicket(arrayProductos) {
   let totalElement = document.createElement('p');
   totalElement.innerText = `Total: $${subtotal + aplicarIva(subtotal)}`;
   ticket.appendChild(totalElement);
-  
 
 }
 
-//Declaracion de la clase Producto
+// Funciones Auxiliares
+function aplicarIva(precio) {
+  return precio * 0.21;
+}
+
+function regeneraTicket(ticket) {
+  // Borra el ticket anterior
+  if (ticket.firstChild != null) {
+    let child = ticket.lastElementChild;
+    while (child) {
+      ticket.removeChild(child);
+      child = ticket.lastElementChild;
+    }
+  }
+}
+
+// Declaracion de la clase Producto
 class Producto {
   constructor(nombre, precio, cantidad) {
+    this.id = new Date().getTime();
     this.nombre = nombre.toUpperCase();
     this.precio = Number.parseFloat(precio);
     this.cantidad = Number.parseInt(cantidad);
@@ -109,6 +143,3 @@ class Producto {
     return parseFloat(this.precio * this.cantidad);
   }
 }
-
-// Ejecucion del main
-main();
